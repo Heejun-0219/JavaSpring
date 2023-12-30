962462522f20b855b3a77ceed7e475dfc43f4735
Heejun-0219 <heejun0219@gmail.com>
2023-12-30 14:48:56 +0900

    @Configuration과 바이트크도 조작의 마법
    스프링 컨테이너는 싱글톤 컨테이너이다.
    bean.getClass() = class hello.core.AppConfig$$SpringCGLIB$$0
    
    바이크코드 조작 라이브러리를 사용해서 내가 만든 클래스를 상속받아 임의의 다른 클래스를 만들고 스프링 빈에 등록한다.

--- src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
+++ src/test/java/hello/core/singleton/ConfigurationSingletonTest.java
@@ -30,4 +30,12 @@ public class ConfigurationSingletonTest {
         Assertions.assertThat(memberRepository1).isSameAs(bean);
         Assertions.assertThat(memberRepository2).isSameAs(bean);
     }
+
+    @Test
+    void configurationDeep() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        System.out.println("bean = " + bean.getClass());
+    }
 }
