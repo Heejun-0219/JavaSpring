8b6de5c36f09b733f6253edcafd5e41862b4af2d
Heejun-0219 <heejun0219@gmail.com>
2024-01-05 14:53:39 +0900

    중복 등록과 충돌
    자동 vs 자동 -> 오류 발생
    수동 vs 자동 -> 수동 등록 우선 순위를 가지며 수동 빈이 자동 빈을 오버라이딩한다. (최근 스프링 부트의 경우는 애매한 상황을 막기 위해 오류 발생 시키는 옵션으로 변경되었다. = override disable)

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,7 @@
 package hello.core;
 
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
@@ -15,4 +17,9 @@ import org.springframework.context.annotation.FilterType;
 // @Component가 붙은 모든 클래스를 스프링 빈으로 등록한다.
 // @Configuration이 붙은 클래스는 스프링 빈으로 등록하지 않는다. 예제 코드에서 사용했기 때문이다. 실무에서는 사용하지 않는다.
 public class AutoAppConfig {
+
+    @Bean(name = "memoryMemberRepository")
+    public MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
 }
