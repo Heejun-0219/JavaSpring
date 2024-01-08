a3410fdbb95a8bbee9a923c801c6b56da7e597fd
Heejun-0219 <heejun0219@gmail.com>
2024-01-08 10:18:44 +0900

    인터페이스 InitializingBean, DisposableBean (스프링 초창기)
    InitializingBean - 의존관계 주입이 끝나면 호출
    
    DisposableBean - 빈이 종료될 떄 호출
    
    - 코드를 스프링 전용 인터페이스에 의존적으로 작성해야한다.

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,13 +1,14 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
 
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
     }
 
     public void setUrl(String url) {
@@ -28,16 +29,18 @@ public class NetworkClient {
         System.out.println("close: " + url);
     }
 
-    // 스프링 빈이 생성되고 의존관계 주입이 완료된 후 호출
-    public void init() {
-        System.out.println("NetworkClient.init");
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        // 의존관계 주입이 끝나면 호출
+        System.out.println("NetworkClient.afterPropertiesSet");
         connect();
         call("초기화 연결 메시지");
     }
 
-    // 스프링 빈이 소멸되기 직전에 호출
-    public void close() {
-        System.out.println("NetworkClient.close");
+    @Override
+    public void destroy() throws Exception {
+        // 빈이 종료될 때 호출
+        System.out.println("NetworkClient.destroy");
         disconnect();
     }
 }
