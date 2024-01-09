ec520a236d64a59e34ebf97edbe4043c1a646ee1
Heejun-0219 <heejun0219@gmail.com>
2024-01-09 10:58:50 +0900

    스코프와 프록시
    @Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)
    - 클래스 : TARGET_CLASS
    - 인터페이스 : INTERFACE
    
    MyLogger의 가짜 프록시 클래스를 만들어주고 HTTP request와 상관 없이 가짜 프록시 클래스를 다른 빈에 미리 주입해 둘 수 있다. (class hello.core.common.MyLogger$$SpringCGLIB$$0) - 마치 provider처럼 작용한다. 요청이 들어오면 빈을 찾아서 반환해준다. (위임 로직 존재 - 실제 빈을 찾는 방법을 알고 있다.)
    가짜 프록시 객체의 메서드 -> myLogger.logic()
    원본 클래스를 상속받아서 만들어진다. (싱글톤처럼 등록된다. 싱글톤은 아니다. 사용을 최소화하자.)
    
    ** Provider, 프록시 : 진짜 객체 조회를 필요한 시점까지 지연처리한다 ** - 단지 애노테이션 설정 변경만으로 -> 다형성, DI 강점

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -2,9 +2,7 @@ package hello.core.web;
 
 import hello.core.common.MyLogger;
 import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -14,15 +12,14 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-//    private final MyLogger myLogger;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
+//    private final ObjectProvider<MyLogger> myLoggerProvider;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
         String requestURL = request.getRequestURL().toString();
-        MyLogger myLogger = myLoggerProvider.getObject(); // 호출 시에 request scope 빈을 찾아서 반환 (DL)
-        // 각 호출 마다 다른 request scope 빈이 생성되고, 요청이 끝나면 소멸한다.
+        System.out.println("myLogger = " + myLogger.getClass()); // 프록시 객체
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
--- src/main/java/hello/core/web/LogDemoService.java
+++ src/main/java/hello/core/web/LogDemoService.java
@@ -1,7 +1,6 @@
 package hello.core.web;
 
 import hello.core.common.MyLogger;
-import jakarta.inject.Provider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,10 +8,8 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class LogDemoService {
 
-//    private final MyLogger myLogger;
-    private final Provider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
     public void logic(String id) {
-        MyLogger myLogger = myLoggerProvider.get();
         myLogger.log("service id = " + id);
     }
 }
