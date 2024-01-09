cc126829fafcd6cad675454ab68899bded15096d
Heejun-0219 <heejun0219@gmail.com>
2024-01-09 10:16:46 +0900

    프로토타입 스코프 - 싱글톤 빈과 함께 사용시 Provider로 문제 해결
    필요한 기능 : 지정한 프로토타입 빈을 컨테이너에서 대신 찾아주는 DL의 기능 제공자 (LookUp)
    
    ObjectFactory, ObjectProvider
    
    - Factory - 기능은 단순, 별도의 라이브러리 필요 없음, 스프링에 의존
    - Provider - Factory 상속, 옵션, 스트림 처리등 편의 기능이 많고, 별도의 라이브러리 필요없음, 스프링에 의존
    
    JSR-330 Provider (라이브러리 추가)
    
    자바 표준 vs ObjectProvider
    코드를 스프링이 아닌 다른 컨테이너에서 사용해야 한다면 자바 표준을 사용하자. 아닌 경우에는 스프링이 제공하는 기능을 사용하자.

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
+import jakarta.inject.Provider;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -36,21 +37,29 @@ public class SingletonWithPrototypeTest1 {
         ClientBean clientBean2 = ac.getBean(ClientBean.class); // 새로운 프로토타입 빈이 생성되지 않는다.
         int count2 = clientBean2.logic();
         System.out.println("count2 = " + count2);
-        assertThat(count2).isEqualTo(2); // prototypeBean2.count = 1
+        assertThat(count2).isEqualTo(1); // prototypeBean2.count = 1
 
         ac.close();
     }
 
     @Scope("singleton")
     static class ClientBean {
-        private final PrototypeBean prototypeBean; // 생성 시점에 주입
+//        private final PrototypeBean prototypeBean; // 생성 시점에 주입
+//        private ApplicationContext ac;
 
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        private Provider<PrototypeBean> prototypeBeanProvider;
+        // ObjectProvider: 스프링 컨테이너를 통해 해당 빈을 찾아주는 DL 서비스 제공
+
+//        @Autowired
+//        public ClientBean(PrototypeBean prototypeBean) {
+//            this.prototypeBean = prototypeBean;
+//        }
 
         public int logic() {
+//            PrototypeBean prototypeBean = ac.getBean(PrototypeBean.class); // 요청 시점에 주입
+            PrototypeBean prototypeBean = prototypeBeanProvider.get(); // DL
+            // 핵심 컨셉 : 스프링 컨테이너에 요청해서 새로운 프로토타입 빈을 생성하고, 그대로 반환 -> 프로토타입 빈 사용 -> 스프링 컨테이너가 프로토타입을 만들어서 주입해줌
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }
